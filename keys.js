a23a79dbb9265366df225bdf49dc4117f7f1b2b9
Benjamin Dally <benjamin.dally@icloud.com>
2018-03-28 13:03:16 -0700

    Working twitter, spotify, and inputs

--- keys.js
+++ keys.js
@@ -1,6 +1,4 @@
-console.log("this is loaded");
-
-module.exports.twitter = {
+exports.twitter = {
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
@@ -11,7 +9,3 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET,
 };
-// module.exports = {
-//   essentials: essentials,
-//   nicetohaves: nicetohaves
-// };
--- liri.js
+++ liri.js
@@ -1,3 +1,81 @@
-require(".env").config();
+require("dotenv").config();
 
-console.log(twitter.consumer_key);
+var userInput = process.argv[2];
+var processInput = process.argv;
+var userQuery = "";
+
+for (var i = 3; i < processInput.length; i++) {
+  userQuery += processInput[i] + " ";
+}
+
+console.log(userQuery);
+
+var keys = require("./keys.js");
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
+
+var client = new Twitter(keys.twitter);
+var spotify = new Spotify(keys.spotify);
+
+var params = { screen_name: "benjamindally" };
+
+if (userInput === "spotify-this-song") {
+  spotifyGo();
+} else if (userInput === "my-tweets") {
+  twitterGo();
+} else if (userInput === "movie-this") {
+  omdbGo();
+} else if (userInput === "do-what-it-says") {
+  readFile();
+} else if (userInput === undefined) {
+  console.log(
+    "Do you need a list of commands? Try entering 'my-tweets', 'spotify-this-song', 'movie-this', or 'do-what-it-says'. Or don't, you do you, fam."
+  );
+}
+
+function twitterGo() {
+  client.get("statuses/user_timeline", params, function(
+    error,
+    tweets,
+    response
+  ) {
+    if (!error) {
+      console.log("Here are your last 20 tweets, my dude:");
+      console.log(" ");
+      for (var i = 0; i < 19; i++) {
+        var tweetText = JSON.stringify(tweets[i].text);
+        var tweetCreated = JSON.stringify(tweets[i].created_at);
+        console.log("Your tweet on " + tweetCreated + " said, " + tweetText);
+        console.log(" ");
+      }
+    }
+  });
+}
+
+function spotifyGo() {
+  if (userQuery === undefined) {
+    userQuery = "The Sign";
+  }
+
+  spotify.search({ type: "track", query: userQuery }, function(err, data) {
+    if (err) {
+      return console.log("Error occurred: " + err);
+    }
+
+    var lowerName = data.tracks.items[0].artists[0].name;
+    var upperName = lowerName.charAt(0).toUpperCase() + lowerName.substring(1);
+    var albumName = data.tracks.items[0].album.name;
+    var songName = data.tracks.items[0].name;
+    var previewURL = data.tracks.items[0].external_urls.spotify;
+    console.log(" ");
+    console.log("Here's what I found about your song:");
+    console.log(" ");
+    console.log("Artist: " + upperName);
+    console.log(" ");
+    console.log("Song Name: " + songName);
+    console.log(" ");
+    console.log("Check it out: " + previewURL);
+    console.log(" ");
+    console.log("Album: " + albumName);
+  });
+}
